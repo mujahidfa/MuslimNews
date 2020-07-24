f26c0ecba2dd62684de74f8d898e59d3bc8802b6
Riza Hassan <ruhulruzbihan@gmail.com>
2020-07-24 17:41:47 -0500

    Edit about and team page

--- pages/about.vue
+++ pages/about.vue
@@ -1,6 +1,64 @@
 <template>
-  <main class="min-h-screen flex justify-center text-center mx-auto">
-    About
+  <main class="min-h-screen justify-center text-center mx-12 lg:mx-48">
+    <h1 class="mt-8 font-serif font-bold text-3xl md:text-4xl text-gray-800">
+      MuslimNews
+    </h1>
+    <div class="p-12 text-justify">
+      <div class="text-center">
+        <h2 class="text-gray-700 text-lg md:text-2xl">
+          MuslimNews is an open-source platform that gather all Islam and Muslim
+          related news articles from around the world. It was developed to help
+          news readers find relevant articles that suits their interest.
+        </h2>
+      </div>
+
+      <div class="mt-24 border-t-2 border-b-2">
+        <h1 class="font-bold text-indigo-600 text-xl md:text-3xl">
+          Articles
+        </h1>
+        <h2 class="text-gray-700 text-lg md:text-xl">
+          Muslim and Islam related news article are collected using
+          <a class="text-gray-900 hover:underline" href="https://newsapi.org/"
+            >an open source API</a
+          >
+          . It gets news headlines and articles from news sources and blogs all
+          around the world.
+        </h2>
+      </div>
+      <div class="mt-12 border-t-2 border-b-2">
+        <h1 class="font-bold text-indigo-600 text-xl md:text-3xl">
+          Features
+        </h1>
+        <h2 class="text-gray-700 text-lg md:text-xl">
+          Readers will be able to search for news using keywords, then sort them
+          by published date, popularity, and relevancy. Popular categories are
+          pre-determined according to trending news.
+        </h2>
+      </div>
+      <div class="mt-12 border-t-2 border-b-2">
+        <h1 class="font-bold text-indigo-600 text-xl md:text-3xl">
+          History
+        </h1>
+        <h2 class="text-gray-700 text-lg md:text-xl">
+          This platform was developed for a hackathon- UmmaHacks, the world's
+          first virtual Muslim Hackathon in the Summer of 2020. It was developed
+          by Malaysians
+          <a
+            class="text-gray-900 hover:underline"
+            href="mailto:mujahidfanuar@gmail.com"
+            >Mujahid Anuar</a
+          >
+          and
+          <a
+            class="text-gray-900 hover:underline"
+            href="mailto:ruhulruzbihan@gmail.com"
+            >Riza Hassan</a
+          >
+          who are interested in making Islam and Muslim related news more
+          reachable.
+        </h2>
+      </div>
+    </div>
   </main>
 </template>
 
--- pages/team.vue
+++ pages/team.vue
@@ -1,6 +1,35 @@
 <template>
-  <main class="min-h-screen flex justify-center text-center mx-auto">
-    Team
+  <main class="min-h-screen justify-center text-center px-48">
+    <div>
+      <h1 class="mt-8 font-serif font-bold text-3xl md:text-4xl text-gray-800">
+        Meet the team
+      </h1>
+    </div>
+
+    <div class="flex flex-col items-center justify-center mt-12">
+      <div class="rounded-full w-48 h-48 border-2">
+        Image
+      </div>
+      <div class="">
+        <h2 class="text-2xl font-semibold">Mujahid Anuar</h2>
+        <h2 class="text-xl">
+          A thinker with web & mobile apps, and loves everything beautiful and
+          open source.
+        </h2>
+        <div class="flex flex-row">
+          <GithubIcon class="w-8" /> <InstaIcon class="w-8" />
+        </div>
+      </div>
+      <div class="mt-12 rounded-full w-48 h-48 border-2">
+        Image
+      </div>
+      <div class="">
+        <h2 class="text-2xl font-semibold">Riza Hassan</h2>
+        <h2 class="text-xl">
+          Computer Science student interested in Entrepreneurship
+        </h2>
+      </div>
+    </div>
   </main>
 </template>
 
